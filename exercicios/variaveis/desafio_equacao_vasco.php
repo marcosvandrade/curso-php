99e304363e055d1142f8dd06a41e9246fdb03215
marcosvandrade <vascorj2006@gmail.com>
2023-01-07 13:33:13 -0300

    challenge equation vasco

---
+++ exercicios/variaveis/desafio_equacao_vasco.php
@@ -0,0 +1,12 @@
+<?php
+
+$numeradorA = (6 * (3+2)) ** 2;
+$denominadorA = (3*2);
+$B = (((1-5) * (2-7)) / 2) ** 2;
+$denominadorEq = (10 ** 3);
+
+$resultadoA = $numeradorA/$denominadorA;
+
+echo (($resultadoA - $B) ** 3) / (10 ** 3);
+
+?>
