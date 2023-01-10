e55bdb2374185ecbeaf1b8c662f0f4114eaf041e
marcosvandrade <vascorj2006@gmail.com>
2023-01-10 15:47:46 -0300

    challenge variaveis variaveis conclude

---
+++ exercicios/variaveis/desafio_variaveis_vasco.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 'Nossa';
+$Nossa = 'Eu';
+$Eu = "consegui";
+$consegui = 'responder';
+$responder = 'esse';
+$esse = 'desafio';
+
+echo "$a! {$$a} {$$$a} {$$$$a} {$$$$$a} {$$$$$$a}.";
+
+?>
